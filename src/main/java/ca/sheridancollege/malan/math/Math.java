656f64ebed67f0bb94fedbf1e8e13dbdb37f1ec1
Christian Malan <malanc@sheridancollege.ca>
2026-01-28 14:18:26 -0500

    Initial commit

---
+++ src/main/java/ca/sheridancollege/malan/math/Math.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package ca.sheridancollege.malan.math;
+
+/**
+ *
+ * @author Christian Malan
+ */
+public class Math {
+    
+    //this is an initial file with initial class and method
+     public static int add(int a, int b) {
+        return a + b;
+    }
+}
